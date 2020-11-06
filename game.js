93a1044b6763fb566ad3f545262c4e3a5eb2bafa
Cody Durham <codydd@gmail.com>
2020-11-05 21:16:50 -0700

    Initial commit

---
+++ game.js
@@ -0,0 +1,28 @@
+class Game {
+  constructor () {
+  this.playerOne = new PlayerOne(id, token, wins);
+  this.playerTwo = new PlayerTwo(id, token, wins);
+  this.data = // not sure what to put here
+  this.playerTurn = 'one' || 'two';
+
+
+  }
+
+winConditions() {
+  //will check to see if there are 3 squares in a row
+  //if playerOne
+}
+
+drawConditions() {
+  //this will check if no squares are in a row
+}
+
+saveWinToStorage() {
+
+}
+
+resetGameBoard() {
+
+}
+
+};
